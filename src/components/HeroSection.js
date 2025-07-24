b24be378665dd8c591662bc53914251e4b1e5fdf
ssalih755 <sarrahsalih2@gmail.com>
2025-07-24 18:26:29 -0400

    fixed HeroSection formatting and restructured project cards

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,11 +7,18 @@ function HeroSection() {
     <section className="heroCard">
     <WorkCard
     title="Hello and Welcome!"
-    description="I'm a Full Stack Java Developer with a proven background as a Software Quality Assurance Lead. I recently completed an intensive coding bootcamp at Tech Elevator, where I built full-stack applications using Java, Spring Boot, PostgreSQL, JavaScript, React, and Bootstrap.
-    What sets me apart is my foundation in quality-first thinking. With years of experience leading QA efforts across production systems, I bring a deep understanding of how to build software that is robust, testable, and user-focused. I anticipate edge cases, design for maintainability, and bring a disciplined approach to both development and collaboration.
-    In transitioning to a full-stack role, I've combined my strengths in testing, problem-solving, and team communication with hands-on development experience. My projects reflect not only technical ability but also an attention to detail and a commitment to delivering reliable, scalable solutions.
-    If my background or work resonates with you, feel free to explore my repositories or connect—I'm always open to thoughtful conversations about technology, development, and building better software.
+    description={
+      <>
+      I'm a Full Stack Java Developer with a proven background as a Software Quality Assurance Lead. I recently completed an intensive coding bootcamp at Tech Elevator, where I built full-stack applications using Java, Spring Boot, PostgreSQL, JavaScript, React, and Bootstrap.
+    <br /><br />
+    What sets me apart is my foundation in quality-first thinking. With years of experience leading QA efforts across production systems, I bring a deep understanding of how to build software that is robust, testable, and user-focused. I anticipate edge cases, design for maintainability, and approach development with a disciplined, collaborative mindset.
+    <br /><br />
+    In transitioning to a full-stack role, I've combined my strengths in testing, problem-solving, and team communication with hands-on development experience. My projects reflect not only technical ability but also a strong attention to detail and a commitment to delivering reliable, scalable solutions.
+    <br /><br />
+    If my background or work resonates with you, feel free to explore my portfolio or connect—I'm always open to thoughtful conversations about technology, development, and building better software.
     "
+    </>
+    }
     />
     </section>
   );
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,29 +1,91 @@
-import React from 'react';
-import WorkCard from './WorkCard';
-import '../styles/ProjectsSection.css';
+import React from "react";
+import WorkCard from "./WorkCard";
+import "../styles/ProjectsSection.css";
 
 function ProjectsSection() {
   return (
     <section id="projects" className="projects-section">
       <h2>Projects</h2>
       <div className="cards-grid">
-        <WorkCard className="project-card"
+        <WorkCard
+          className="project-card"
           title="Cake Ordering Website"
-          description="A full-stack web application for customers to browse cakes, customize selections, and place pickup orders online. Built with React for the frontend and Axios 
-          for API integration. Backend developed using Java and Spring Boot with a PostgreSQL database to manage products, orders, and user accounts securely. Implements a 
-          service-based architecture, React Context for state management, and reusable components for a smooth checkout and order summary experience. Includes user authentication, 
-          secure API endpoints, and transaction logging for reliability.A full-stack site for browsing and ordering custom cakes. Built with React, Java, Spring Boot, PostgreSQL."
+          description={
+            <>
+              <strong>Description: </strong> <br /> <br />
+              A full-stack web application that allows customers to browse
+              cakes, customize selections, and place pickup orders online. It
+              features a smooth checkout flow, reusable components, user
+              authentication, secure API endpoints, and transaction logging for
+              reliability.
+              <br /> <br />
+              <strong>Tech Stack</strong>
+              <br /> <br />
+              <strong>Frontend:</strong> React, React Context, Axios
+              <br /> <br />
+              <strong>Backend:</strong> Java, Spring Boot
+              <br /> <br />
+              <strong>Database:</strong> PostgreSQL
+              <br /> <br />
+              <strong>Architecture:</strong> Service-based
+              <br /> <br />
+              <strong>Other Features:</strong> Reusable components, secure API,
+              user authentication, transaction logging
+            </>
+          }
         />
-        <WorkCard className="project-card"
+        <WorkCard
+          className="project-card"
           title="Personal Portfolio Website"
-          description="A fully responsive portfolio site built with React and CSS to showcase my development projects and technical skills. 
-          Implements modern UI features like custom radial gradients, draggable project cards with react-draggable, and clean component architecture. Focused on maintainable code, reusable components, and an engaging user experience."
+          description={
+            <>
+              <strong>Description:</strong>
+              <br />
+              <br />
+              A fully responsive website built to showcase my development
+              projects and technical background. Designed with a focus on clean
+              component architecture and maintainable code.
+              <br />
+              <br />
+              <strong>Frontend:</strong> React and CSS
+              <br />
+              <br />
+              <strong>Other Features: </strong>
+              Custom radial gradients, draggable project cards using{" "}
+              react-draggable, reusable components, and smooth user interactions
+              for an engaging layout that reflects both technical skills and
+              design sensibility.
+            </>
+          }
         />
-        <WorkCard className="project-card"
-          title="Vending Machine App"
-          description="A fully functional console-based vending machine built with Java. This project handles product inventory from an input file, accepts customer payments, 
-          dispenses items, calculates change, and securely logs all transactions. It restocks automatically on each run and is thoroughly tested with JUnit to ensure robust 
-          performance."
+        <WorkCard
+          className="project-card"
+          title="Vending Machine CLI App"
+          description={
+            <>
+              <strong>Description:</strong>
+              <br />
+              <br />
+              A Java-based console application that simulates a vending machine.
+              It manages inventory from an input file, accepts payments,
+              dispenses items, calculates change, and logs transactions
+              securely. Automatically restocks on each run for consistent
+              testing and use.
+              <br />
+              <br />
+              <strong>BackEnd:</strong>
+              <br />
+              Java, File I/O, Object-Oriented Programming (OOP), Exception
+              Handling
+              <br />
+              <br />
+              <strong>Other Features:</strong>
+              <br />
+              Automated restocking, transaction logging for audit trails, and
+              robust unit testing using JUnit to ensure reliability and
+              edge-case coverage.
+            </>
+          }
         />
       </div>
     </section>
